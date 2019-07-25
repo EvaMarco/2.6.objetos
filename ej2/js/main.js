8e9b6520ee9a686bec13c95b4199aa1471b78165
Migara <evamarcod@gmail.com>
2019-07-25 08:59:24 +0200

    Add exercise 2

---
+++ ej2/js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+// const adalaber1 = {};
+// adalaber1.name = 'Susana';
+// adalaber1.age = '34';
+// adalaber1.profession = 'periodista';
+
+// adalaber1.run = () => console.log ('Estoy corriendo');
+
+
+
+const adalaber1 = {
+    name : 'Susana',
+    age : '34',
+    profession : 'periodista',
+    run : () => console.log ('Estoy corriendo')
+};
+
+adalaber1.run();
